4753c606260179702187557816fe2792bc88305e
space-inside <zhenya.filina@mail.ru>
2020-01-15 11:10:05 +0300

    task 17

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-const element = (<div>
+const ItemList = () => (<ul>
+    <li>Первый пункт</li>
+    <li>Второй пункт</li>
+    <li>Третий пункт</li>
+</ul>);
+const App = () => (<div>
     <h1>Hello World!</h1>
-    <p>Good luck!</p>
+    <ItemList />
 </div>);
-
-export {element};
+export default App;
