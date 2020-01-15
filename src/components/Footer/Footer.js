3b5e7936fa9389023ff4afa0505f9d09bae69c55
space-inside <zhenya.filina@mail.ru>
2020-01-15 12:05:27 +0300

    task 18

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const Footer = () => (
+    <span>Developed by WebHeroSchool</span>
+);
+export default Footer;
---
+++ src/components/InputItem/InputItem.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const InputItem = () => (
+    <input placeholder={"What need to be done?"}></input>
+);
+export default InputItem;
