66a9c1346321c392c98e51f2cda4bc0cca360202
space-inside <zhenya.filina@mail.ru>
2020-01-17 14:22:53 +0300

    task 26

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,8 +5,7 @@ const Item = ({value, isDone}) => (
     <span className={classnames({
         [styles.item]:true,
         [styles.done]:isDone
-    })
-    }>
+    })}>
         {value}
     </span>
 );
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
 import Item from '../Item/Item';
+import styles from './ItemList.module.css';
+import './ItemList.css';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 const ItemList = ({items}) => (<ul>
-    {items.map((item) => (<li key={item.value}><Item value={item.value} isDone={item.isDone}/></li>))}
+    {items.map((item) => (<li className={styles.item_list} key={item.value}>
+        <FormControlLabel
+        control={
+            <Checkbox
+                checked={item.isDone}
+                value="checkedB"
+                color="primary"
+            />
+        }
+        label={<Item value={item.value} isDone={item.isDone}/>}
+        />
+        <Tooltip title="Удалить">
+            <IconButton aria-label="delete" classes={{ label: 'button__delete' }}>
+                <DeleteIcon fontSize="small" />
+            </IconButton>
+        </Tooltip>
+    </li>))}
 </ul>);
 export default ItemList;
