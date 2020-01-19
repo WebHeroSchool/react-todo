e4f89b7513073e2c6044fbbe6690665b6141a938
space-inside <zhenya.filina@mail.ru>
2020-01-20 01:02:26 +0300

    added media querries

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Input from '@material-ui/core/Input';
+import styles from './InputItem.module.css'
 const InputItem = () => (
-    <Input placeholder="Что нужно сделать?" inputProps={{ 'aria-label': 'description' }} />
+    <Input placeholder="Что нужно сделать?" classes={{ input: [styles.input] }} inputProps={{ 'aria-label': 'description' }} />
 );
 export default InputItem;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,12 +2,11 @@ import React from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
-import './ItemList.css';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-const ItemList = ({items}) => (<ul>
+const ItemList = ({items}) => (<ul className={styles.list}>
     {items.map((item) => (<li className={styles.item_list} key={item.value}>
         <FormControlLabel
         control={
@@ -20,7 +19,7 @@ const ItemList = ({items}) => (<ul>
         label={<Item value={item.value} isDone={item.isDone}/>}
         />
         <Tooltip title="Удалить">
-            <IconButton aria-label="delete" classes={{ label: 'button__delete' }}>
+            <IconButton aria-label="delete" classes={{ label: [styles.button__delete] }}>
                 <DeleteIcon fontSize="small" />
             </IconButton>
         </Tooltip>
