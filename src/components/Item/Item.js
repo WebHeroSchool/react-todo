003034c2ef45ec5053bb250c195df6bddfb9628a
space-inside <zhenya.filina@mail.ru>
2020-01-20 18:59:05 +0300

    minor fix

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
 import styles from './Item.module.css';
-const Item = ({value, isDone, onClickDone, id}) => (
+const Item = ({value, isDone}) => (
     <span className={classnames({
         [styles.item]:true,
         [styles.done]:isDone
