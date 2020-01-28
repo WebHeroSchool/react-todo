67bd9d4d4d366443f58f9a21cef2195b492957ae
space-inside <zhenya.filina@mail.ru>
2020-01-28 18:50:39 +0300

    task 38

---
+++ src/components/About/About.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './About.module.css';
+const About = () => {
+    return <h1 className={styles.title}>About</h1>
+};
+export default About;
---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Contacts.module.css';
+const Contacts = () => {
+    return <h1 className={styles.title}>Contacts</h1>
+};
+export default Contacts;
